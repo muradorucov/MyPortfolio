c86ed65858386315a1a583ced01330176844c65d
Murad685 <orucowmurad@gmail.com>
2021-07-14 12:16:44 +0400

    Portfolio end first

--- assets/js/lib/validate.js
+++ assets/js/lib/validate.js
@@ -18,7 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
+        displayError(thisForm, 'Bağışlayın form müddəti işləmir!')
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
